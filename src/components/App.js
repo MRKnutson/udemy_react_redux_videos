4daf5ce6901e348cb373e478f5729bed7f469bdd
Michael Knutson <mrknutson44@gmail.com>
2022-05-26 11:01:30 -0800

    linked up to API

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import youtube from "../apis/youtube";
 import SearchBar from "./SearchBar";
+import VideoList from "./VideoList";
 
 function App() {
+  const [videos, setVidoes] = useState([]);
+
+  const onTermSubmit = async (term) => {
+    let response = await youtube.get("/search", {
+      params: {
+        q: term,
+      },
+    });
+    setVidoes(response.data.items);
+  };
+
   return (
     <div className="ui container">
-      <SearchBar />
+      <SearchBar onFormSubmit={onTermSubmit} />
+      <VideoList videos={videos} />
     </div>
   );
 }
