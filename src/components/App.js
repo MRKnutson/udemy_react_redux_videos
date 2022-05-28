86233d5d51a85944b154f0882f1f54ca9da069ee
Michael Knutson <mrknutson44@gmail.com>
2022-05-28 15:17:44 -0800

    displaying video search results

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import youtube from "../apis/youtube";
 import SearchBar from "./SearchBar";
+import VideoDetail from "./VideoDetail";
 import VideoList from "./VideoList";
 
 function App() {
   const [videos, setVidoes] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
+  useEffect(() => {
+    onTermSubmit("truck");
+  }, []);
 
   const onTermSubmit = async (term) => {
     let response = await youtube.get("/search", {
@@ -13,12 +19,26 @@ function App() {
       },
     });
     setVidoes(response.data.items);
+    setSelectedVideo(response.data.items[0]);
+  };
+
+  const onVideoSelect = (video) => {
+    setSelectedVideo(video);
   };
 
   return (
     <div className="ui container">
       <SearchBar onFormSubmit={onTermSubmit} />
-      <VideoList videos={videos} />
+      <div className="ui grid">
+        <div className="ui row">
+          <div className="eleven wide column">
+            <VideoDetail video={selectedVideo} />
+          </div>
+          <div className="five wide column">
+            <VideoList videos={videos} onVideoSelect={onVideoSelect} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,7 +1,19 @@
 import React from "react";
+import VideoItem from "./VideoItem";
 
-const VideoList = ({ videos }) => {
-  return <div>Video List: {videos.length}</div>;
+const VideoList = ({ videos, onVideoSelect }) => {
+  const renderVideoList = () => {
+    return videos.map((video) => {
+      return (
+        <VideoItem
+          key={video.id.videoId}
+          video={video}
+          onVideoSelect={onVideoSelect}
+        />
+      );
+    });
+  };
+  return <div className="ui relaxed divided list">{renderVideoList()}</div>;
 };
 
 export default VideoList;
